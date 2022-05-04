1533e1cec78250fc33276cfa39fb4cd63d8df4eb
DouglasMarq <douglas.marq.alves@outlook.com>
2022-05-03 21:07:32 -0300

    feat: adding tests

--- utils/messages_utils_test.go
+++ utils/messages_utils_test.go
@@ -1,1 +1,14 @@
-package utils_test
+package utils
+
+import "testing"
+
+func TestMessageParemeter(t *testing.T) {
+
+	m := "!clear 50"
+
+	result := GetMessageParameter(m)
+
+	if result < 1 {
+		t.Errorf("Expected number of messages to not be 0, but got %v", result)
+	}
+}
